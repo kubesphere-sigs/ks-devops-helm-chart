c5e4f90975e668c9b1fe74a799773709aed32af9
rick <1450685+LinuxSuRen@users.noreply.github.com>
2021-09-13 13:47:52 +0800

    Update CRDs and provide a script to fetch CRDs

---
+++ hack/fetch-crds.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+crds=("devops.kubesphere.io_devopsprojects.yaml devops.kubesphere.io_pipelines.yaml devops.kubesphere.io_pipelineruns.yaml")
+for crd in $crds
+do
+    curl -o charts/ks-devops/crds/$crd https://ghproxy.com/https://raw.githubusercontent.com/kubesphere/ks-devops/master/config/crd/bases/$crd
+done
+
+crds=("devops.kubesphere.io_s2ibuilders.yaml devops.kubesphere.io_s2ibuildertemplates.yaml devops.kubesphere.io_s2iruns.yaml")
+for crd in $crds
+do
+    curl -o charts/ks-devops/charts/s2i/crds/$crd https://ghproxy.com/https://raw.githubusercontent.com/kubesphere/s2ioperator/master/config/crds/$crd
+done
+
+crds=("devops.kubesphere.io_s2ibinaries.yaml")
+for crd in $crds
+do
+    curl -o charts/ks-devops/charts/s2i/crds/$crd https://ghproxy.com/https://raw.githubusercontent.com/kubesphere/ks-devops/master/config/crd/bases/$crd
+done
